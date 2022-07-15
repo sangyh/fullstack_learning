6bb0d34ef19acce1f89f50a9c5f1eae3e7b45d5a
sangyh <sangyh892@gmail.com>
2022-07-14 23:45:00 -0700

    Setting up register endpoint

--- server/src/app.js
+++ server/src/app.js
@@ -2,7 +2,7 @@ console.log("hello")
 const express = require('express')
 const bodyParser = require('body-parser') //process json data easily
 const cors = require('cors') //allows other clients to access our endpoints; could be security risk
-const morgan = require('morgan') //log
+const morgan = require('morgan') //log 
 
 const app = express()
 
@@ -10,9 +10,9 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req,res) => {
+app.post('/register', (req, res) => {
     res.send({
-        message: 'hello world!'
+        message: `Hello ${req.body.email}! Your user was registered!`
     })
 })
 
