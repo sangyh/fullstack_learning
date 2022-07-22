76458a7950ff299e05c62009b048d0987177ec16
sangyh <sangyh892@gmail.com>
2022-07-21 23:53:39 -0700

    set up vuex store to track app states

--- client/src/main.js
+++ client/src/main.js
@@ -4,17 +4,22 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
+import { sync } from 'vuex-router-sync'
 import 'vuetify/dist/vuetify.min.css'
+import store from '@/store/store'
 
 Vue.config.productionTip = false
 
 Vue.use(Vuetify)
 // Vue.use(VueRouter)
 
+sync(store, router) //sync store to the router
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   vuetify: new Vuetify(),
   components: { App },
   template: '<App/>'
